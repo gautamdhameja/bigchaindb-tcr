739fdc70c7bdd67793aed0b0358613190de51cf4
Gautam Dhameja <gautamdhameja@users.noreply.github.com>
2018-09-02 21:18:56 +0200

    removed app

--- src/tcr/lib/registry.js
+++ src/tcr/lib/registry.js
@@ -3,6 +3,13 @@ import * as constants from '../shared/constants'
 import * as token from './token'
 import * as config from './config'
 
-export async function completeProposal() {
-    
+export async function completeProposal(passphrase, proposalId) {
+    //step 1: verify user
+    const user = bdb.getKeypairFromPassphrase(passphrase)
+    const tcr = await bdb.getTransaction(process.env.TCR_ASSET_ID)
+    if (tcr && tcr.inputs[0].owners_before[0] === user.publicKey) {
+        const proposal = await bdb.getTransaction(proposalId)
+    } else {
+        throw new Error ("Access Denied.")
+    }
 }
